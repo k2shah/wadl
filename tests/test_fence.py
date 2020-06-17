4797b280139ffad0644f5265db47a903293d154d
Kunal Shah <kshah.kunal@gmail.com>
2020-06-16 17:06:13 -0700

    up dated maze test

--- tests/test_fence.py
+++ tests/test_fence.py
@@ -1,10 +1,6 @@
 import pytest
 # os
 import os
-# math
-import numpy as np
-import numpy.linalg as la
-import numpy.random as rand
 # plot
 import matplotlib.pyplot as plt
 
@@ -14,7 +10,7 @@ def croz():
     # build fixture
     from wadl.lib.fence import Fence
     # cros test fixture
-    path = os.path.join(os.path.dirname( __file__ ), '..', 'wadl', 'data', 'geofences')
+    path = os.path.join(os.path.dirname( __file__ ), 'data')
     file = os.path.join(path, "croz_west")
     absfile = os.path.abspath(file)
     return Fence(absfile)
