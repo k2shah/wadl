7f3d1e0a07047cbc31a90ccb65906a3af6049d4e
Kunal Shah <kshah.kunal@gmail.com>
2020-06-19 15:06:07 -0700

    working graph merger

--- wadl/graph/multiGraph.py
+++ wadl/graph/multiGraph.py
@@ -1,5 +1,8 @@
 # os
 import os
+# gen
+from collections import defaultdict
+from copy import deepcopy
 # math
 import numpy as np
 from math import floor, ceil
@@ -16,11 +19,11 @@ class MultiGraph(object):
     """docstring for MultiGraph"""
     def __init__(self, graph):
         self.baseGraph = graph
-        self.subgraphs = self.splitGraph_square()
+        self.subGraphs = self.splitGraph()
 
 
 
-    def splitGraph_square(self, size=40):
+    def splitGraph(self, size=40):
         """splits a graph into sub segments
         size: aprox number of nodes in each sub graph
         """
@@ -57,7 +60,8 @@ class MultiGraph(object):
         xStep = ceil(xDelta/xBlock)
         yStep = ceil(yDelta/yBlock)
 
-        subNodes = [[] for _ in range(xBlock * yBlock)]
+        subNodes = defaultdict(list)
+        node2sub = dict() # map from node to it's subgraph
         for node in self.baseGraph.nodes:
             rNode = (node[0] - xBound[0],
                      node[1] - yBound[0] ) # get node reltative to bottom left
@@ -68,12 +72,45 @@ class MultiGraph(object):
             # print(node, rNode, group, subGraphIdx)
             # add to the list
             subNodes[subGraphIdx].append(node)
+            node2sub[node] = subGraphIdx
 
-        subgraphs = [self.baseGraph.subgraph(nodes) for nodes in subNodes if nodes]
-        print(f"Found {len(subgraphs)} subgraps with max block size of {xStep * yStep}")
+        subNodes = self.mergeSubgraphs(subNodes, node2sub)
+        # there has to be a better way to do this
+        maxNodes = len(max(subNodes.values(), key= lambda x: len(x)))
+        minNodes = len(min(subNodes.values(), key= lambda x: len(x)))
 
-        return subgraphs
+        subGraphs = [self.baseGraph.subgraph(nodes) for  nodes in subNodes.values()]
+        print(f"Found {len(subGraphs)} subgraphs with {minNodes} to {maxNodes} nodes")
 
+        return subGraphs
+
+
+    def mergeSubgraphs(self, subNodes, node2sub, mergeSize = 10, maxSize = 60):
+            # merge into the subgraph with the most conections, if tie pick the smallest suubgraph  
+            mergedNodes = deepcopy(subNodes)
+            for i, nodes in subNodes.items():
+                if len(nodes) < mergeSize:
+                    # keep running score of best merge candidant
+                    mergeScore = defaultdict(int)
+                    for node in nodes:
+                        for adj in self.baseGraph.neighbors(node):
+                            adjIdx = node2sub[adj]
+                            if adjIdx != i:
+                                mergeScore[adjIdx] += 1
+                    # sort candiates and merge into the best one
+                    print(i, nodes, mergeScore)
+                    for k in sorted(mergeScore, key=mergeScore.get, reverse=True):
+                        if len(mergedNodes[k]) + len(nodes) < maxSize:
+                            # merge the ith subgraph into the kth subgraph
+                            print(f"Merging subgraph {i} with {len(nodes)} nodes into subgraph {k}")
+                            mergedNodes[k] += nodes
+                            mergedNodes.pop(i)
+                            break
+
+
+
+            return mergedNodes
+            
 
 
 
@@ -101,7 +138,7 @@ if __name__ == '__main__':
     fig, ax = plt.subplots()
     colors = ('r', 'g', 'b', 'm', 'c', 'y', 'k')
 
-    for i, graph in enumerate(mGraph.subgraphs):
+    for i, graph in enumerate(mGraph.subGraphs):
             # print(graph.nodes)
             colIdx = i % len(colors) 
             # print(colors[colIdx])
