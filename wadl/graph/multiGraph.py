e3ff73be1dae872870d8ceae1802463ca10faaf2
Kunal Shah <kshah.kunal@gmail.com>
2020-08-04 23:24:07 -0700

    working path checker

--- wadl/graph/multiGraph.py
+++ wadl/graph/multiGraph.py
@@ -6,15 +6,17 @@ from copy import deepcopy
 # math
 import numpy as np
 from math import floor, ceil
-#graph 
+# graph
 import networkx as nx
 # plot
 import matplotlib.pyplot as plt
 # lib
 from wadl.lib.utils import *
 
+
 class MultiGraph(object):
     """docstring for MultiGraph"""
+
     def __init__(self, graph, size=40):
         self.baseGraph = graph
         self.subGraphs = self.splitGraph(size)
@@ -25,10 +27,9 @@ class MultiGraph(object):
                 graph.nodes[node]['index'] = i
                 self.baseGraph.nodes[node]['subgraph'] = gIdx
 
-
     def getExtends(self, graph):
-        xSort = sorted(graph.nodes, key= lambda x :x[0])
-        ySort = sorted(graph.nodes, key= lambda x :x[1])
+        xSort = sorted(graph.nodes, key=lambda x: x[0])
+        ySort = sorted(graph.nodes, key=lambda x: x[1])
 
         xBound = (xSort[0][0], xSort[-1][0])
         yBound = (ySort[0][1], ySort[-1][1])
@@ -46,25 +47,25 @@ class MultiGraph(object):
         # print(self.baseGraph.nodes)
         # print(xBound, yBound)
 
-        #get deltas
-        xDelta = xBound[-1] - xBound[0] +1 
-        yDelta = yBound[-1] - yBound[0] +1 # +1 for inclusive
-        
+        # get deltas
+        xDelta = xBound[-1] - xBound[0] + 1
+        yDelta = yBound[-1] - yBound[0] + 1  # +1 for inclusive
+
         # find number of subGraphs
-        nNode = xDelta * yDelta 
+        nNode = xDelta * yDelta
         nSubGraph = int(nNode/size)
         # print(nSubGraph)
 
         # print('delta', xDelta, yDelta)
-        ar = yDelta/xDelta # aspect raito of graph
+        ar = yDelta/xDelta  # aspect raito of graph
         # print('ar', ar)
 
-        # calculate how many blocks on each axis, trying to be as square as posible 
-        xBlock = (nSubGraph/ar) **.5
+        # calculate how many blocks on each axis, be as square as posible
+        xBlock = (nSubGraph/ar) ** .5
         yBlock = ar*xBlock
         # round up
-        xBlock = int(xBlock) +1
-        yBlock = int(yBlock) +1
+        xBlock = int(xBlock) + 1
+        yBlock = int(yBlock) + 1
         # print(xBlock, yBlock)
 
         # get steps on each
@@ -72,11 +73,12 @@ class MultiGraph(object):
         yStep = ceil(yDelta/yBlock)
 
         subNodes = defaultdict(list)
-        node2sub = dict() # map from node to it's subgraph
+        node2sub = dict()  # map from node to it's subgraph
         for node in self.baseGraph.nodes:
             rNode = (node[0] - xBound[0],
-                     node[1] - yBound[0] ) # get node reltative to bottom left
-            group = (int(rNode[0]/xStep),  int(rNode[1]/yStep)) # map square index to linear index
+                     node[1] - yBound[0])  # get node reltative to bottom left
+            # map square index to linear index
+            group = (int(rNode[0]/xStep),  int(rNode[1]/yStep))
 
             # get the index of the subgraph
             subGraphIdx = sub2ind(group, (xBlock, yBlock))
@@ -87,16 +89,19 @@ class MultiGraph(object):
 
         subNodes = self.mergeSubgraphs(subNodes, node2sub)
         # there has to be a better way to do this
-        maxNodes = len(max(subNodes.values(), key= lambda x: len(x)))
-        minNodes = len(min(subNodes.values(), key= lambda x: len(x)))
+        maxNodes = len(max(subNodes.values(), key=lambda x: len(x)))
+        minNodes = len(min(subNodes.values(), key=lambda x: len(x)))
 
-        subGraphs = [self.baseGraph.subgraph(nodes) for  nodes in subNodes.values()]
-        print(f"found {len(subGraphs)} subgraphs with {minNodes} to {maxNodes} nodes")
+        subGraphs = [self.baseGraph.subgraph(
+            nodes) for nodes in subNodes.values()]
+        print(f"found {len(subGraphs)} subgraphs with"
+              f"{minNodes} to {maxNodes} nodes")
 
         return subGraphs
 
-    def mergeSubgraphs(self, subNodes, node2sub, mergeSize = 10, maxSize = 60):
-        # merge into the subgraph with the most conections, if tie pick the smallest suubgraph  
+    def mergeSubgraphs(self, subNodes, node2sub, mergeSize=10, maxSize=60):
+        # merge into the subgraph with the most conections
+        # if tie pick the smallest suubgraph
         mergedNodes = deepcopy(subNodes)
         for i, nodes in subNodes.items():
             if len(nodes) < mergeSize:
@@ -106,7 +111,7 @@ class MultiGraph(object):
                     for adj in self.baseGraph.neighbors(node):
                         adjIdx = node2sub[adj]
                         if adjIdx != i:
-                                mergeScore[adjIdx] += 1
+                            mergeScore[adjIdx] += 1
                 # sort candiates and merge into the best one
                 # print(i, nodes, mergeScore)
                 for k in sorted(mergeScore, key=mergeScore.get, reverse=True):
@@ -117,24 +122,24 @@ class MultiGraph(object):
                         mergedNodes.pop(i)
                         break
         return mergedNodes
-            
-    def getCols(self):
-        return iter(plt.cm.rainbow(np.linspace(0,1,len(self))))
 
+    def getCols(self):
+        return iter(plt.cm.rainbow(np.linspace(0, 1, len(self))))
 
     def __iter__(self):
-        return iter(self.subGraphs)  
+        return iter(self.subGraphs)
 
     def __len__(self):
         return len(self.subGraphs)
 
-if __name__ == '__main__':
-    starts = [(0,0),
-              (1,1)]
 
-    path = os.path.join(os.path.dirname( __file__ ), '..', '..', 'tests', 'data')
+if __name__ == '__main__':
+    starts = [(0, 0),
+              (1, 1)]
+
+    path = os.path.join(os.path.dirname(__file__), '..', '..', 'tests', 'data')
     file = os.path.join(path, "croz_west")
-    
+
     absfile = os.path.abspath(file)
     maze = Maze(absfile, starts, rotation=15)
     mGraph = MultiGraph(maze.graph)
@@ -144,7 +149,7 @@ if __name__ == '__main__':
 
     for i, graph in enumerate(mGraph):
             # print(graph.nodes)
-            colIdx = i % len(colors) 
-            # print(colors[colIdx])
-            maze.plotNodes(ax, nodes=graph.nodes, color=colors[colIdx])
+        colIdx = i % len(colors)
+        # print(colors[colIdx])
+        maze.plotNodes(ax, nodes=graph.nodes, color=colors[colIdx])
     plt.show()
--- wadl/lib/path.py
+++ wadl/lib/path.py
@@ -2,41 +2,60 @@
 # import warnings as warn
 import os
 import csv
-import json
-import glob
-import time
-import warnings as warn
 # import sys
 # gis
 import utm
 # math
 import numpy as np
 import numpy.linalg as la
-# plot
-import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d.axes3d as axes3d
-# lib
-from wadl.lib.utils import *
 
 
-class Path(object):
-    """docstring for Path"""
-    def __init__(self, cords, typ="UTM"):
-        self.GPScords = [] # cords in GPS
-        self.homePt = None
-        self.UTMcords = [] # cords in UTM
-        if typ== "UTM":
-            self.setUTM(cords)
-        elif typ=="GPS":
-            self.setGPS(cords)
+class RouteSet(object):
+    """docstring for PathSet"""
+    def __init__(self, home, zone, flightParams):
+        self.home = home
+        self.zone = zone  # store the UTM zone
+        self.flightParams = flightParams
+        self.paths = []
+
+    def check(self, cords):
+        # builds the path from a UTM waypoint list
+        # runs a series of checks to verify the path is viable
+        # returns None if any check fails; the Path otherwise
+        route = Route(cords, self.zone, self.home)
+        route.build(self.flightParams)
+        if route.check():
+            return route
         else:
-            raise RuntimeError("invalid type of coordinates")
+            return None
 
-    def setUTM(self, cords):
-        self.UTMcords = cords
+    def push(self, route):
+        pass
 
-    def setGPS(self, cords):
-        self.GPScord = cords
+
+class Route(object):
+    """docstring for Path"""
+    def __init__(self, cords, zone, home):
+        self.UTMcords = cords  # cords in UTM
+        self.UTMZone = zone
+        self.UTM2GPS(zone)  # set path in GPS (WGS 84)
+        self.setHome(home)  # set home pt (in GPS)
+        self.waypoints = []
+
+        # path limits
+        self.DJIWaypointLimit = 98
+
+    @staticmethod
+    def l2(a, b):
+        # returns the L2 norm of two vectors that are tuples
+        return la.norm(np.array(a) - np.array(b))
+
+    @staticmethod
+    def DistGPS(gps0, gps1, alt0=0, alt1=0):
+        # calculates the distnce in meters of 2 lat/lng points
+        e0, n0, _, _ = utm.from_latlon(*gps0)
+        e1, n1, _, _ = utm.from_latlon(*gps1)
+        return np.linalg.norm([e0-e1, n0-n1, alt0-alt1])
 
     def UTM2GPS(self, zone):
         # converts all the UTM cords to GPS
@@ -46,67 +65,118 @@ class Path(object):
         # converts all the GPS cords to UTM
         self.UTMcords = [utm.from_latlon(*cord)[0:2] for cord in self.GPScords]
 
-    def setHome(self, homePt):
-        # homePT: (lat, long)
+    def setHome(self, home):
+        # home: (lat, long)
         # sets the route home at the home pt
-        self.homePt = np.array(homePt)
-        # finds the cloest pt on the route to the homePT
-        (pt, idx) = min([(l2(homePt, pt), i) for i, pt in enumerate(self.GPScords)])
+        self.home = np.array(home)
+        # finds the cloest pt on the route to the home
+        (pt, idx) = min([(self.l2(home, pt), i)
+                         for i, pt in enumerate(self.GPScords)])
         # shift and wrap
         self.GPScords = self.GPScords[idx:] + self.GPScords[1:idx+1]
-        # sync the utm 
+        # sync the utm
         self.GPS2UTM()
+
+    def check(self):
+        # attempts to merge the segment into the path
+        # returns False if one of the length checks fail
+
+        # check: under the waypoint limit
+        if len(self.waypoints) > self.DJIWaypointLimit:
+            return False
+        # check: under the time limit
+        ToF = 0  # time of flight
+        for wp, nxt in zip(self.waypoints, self.waypoints[1:]):
+            dist = self.DistGPS(wp[0:2], nxt[0:2], wp[3], nxt[3])
+            ToF += dist/wp[4]
+        print(ToF)
+
+        if ToF > self.limit:
+            return False
+        return True
+
+    def build(self, flightParams):
+        # build the path
+
+        # unpack parameters
+        self.limit = flightParams["limit"]
+        spd = flightParams["speed"]
+        alt = flightParams["altitude"]
+        xferSpd = flightParams["xfer_speed"]
+        xferAlt = flightParams["xfer_altitude"]
+        xferAsc = flightParams["xfer_ascend"]
+        xferDes = flightParams["xfer_descend"]
+        landALt = flightParams["land_altitude"]
+
+        # take off
+        Hlat, Hlng = self.home
+        self.waypoints.append(Hlat, Hlng, xferAlt, xferSpd)
+        # get higher above frist point, point camera down
+        lat, lng = self.GPScords[0]
+        self.waypoints.append(lat, lng, xferAlt, xferDes)
+        # push each waypoint
+        for lat, lng in self.GPScords[:-1]:
+            self.waypoints.append(lat, lng, alt, spd)
+        lat, lng = self.GPScords[-1]
+        # last point to ascend to transfer
+        self.waypoints.append(lat, lng, alt, xferAsc)
+        # get higher above last point, point camera fwd
+        self.waypoints.append(lat, lng, xferAlt, xferSpd)
+        # return home
+        self.waypoints.append(Hlat, Hlng, xferAlt, xferDes)
+        # land
+        self.waypoints.append(Hlat, Hlng, landALt, xferDes)
 
     def __len__(self):
         # number of waypoints in path
         return len(self.UTMcords)
 
     def __repr__(self):
-        #print the cords
+        # print the cords
         return print(self.UTMcords)
-        
-    def parseFile(self):
-        pathFiles = glob.glob(os.path.join(self.pathDir, "routes/*"))
-        for file in pathFiles:
-            self.cords = dict()
-            # print(file)
-            with open(file) as csvfile:
-                for line in csv.reader(csvfile, delimiter=','):
-                    if line[2] != '50':
-                        continue
-                    cords = (line[0], line[1])
-                    if cords in self.keyPoints:
-                        continue
-                    try:
-                        self.cords[cords] += 1
-                    except KeyError as e:
-                        self.cords[cords] = 1
-                try:
-                    routeEff = self.calcEff()
-                    print(file, ": ", routeEff)
-                    self.writeEff(file, routeEff)
-                except ZeroDivisionError as e:
-                    print("invalid file: {:s}".format(file))
 
-    def calcEff(self):
-        nPts = 0
-        nPaths = 0
-        for keys in self.cords:
-            nPaths += self.cords[keys]
-            nPts += 1
-        return nPts/nPaths
+    # def parseFile(self):
+    #     pathFiles = glob.glob(os.path.join(self.pathDir, "routes/*"))
+    #     for file in pathFiles:
+    #         self.cords = dict()
+    #         # print(file)
+    #         with open(file) as csvfile:
+    #             for line in csv.reader(csvfile, delimiter=','):
+    #                 if line[2] != '50':
+    #                     continue
+    #                 cords = (line[0], line[1])
+    #                 if cords in self.keyPoints:
+    #                     continue
+    #                 try:
+    #                     self.cords[cords] += 1
+    #                 except KeyError as e:
+    #                     self.cords[cords] = 1
+    #             try:
+    #                 routeEff = self.calcEff()
+    #                 print(file, ": ", routeEff)
+    #                 self.writeEff(file, routeEff)
+    #             except ZeroDivisionError as e:
+    #                 print("invalid file: {:s}".format(file))
 
-    def writeEff(self, routeFile, routeEff):
-        infoFile = os.path.join(self.pathDir, "info.txt")
-        if os.path.exists(infoFile):
-            writeMode = 'a'
-        else:
-            writeMode = 'w'
-        with open(infoFile, writeMode) as f:
-            routeName = routeFile.split('/')[-1]
-            f.write("\n{:s}: {:2.4f}".format(
-                    routeName,
-                    routeEff))
+    # def calcEff(self):
+    #     nPts = 0
+    #     nPaths = 0
+    #     for keys in self.cords:
+    #         nPaths += self.cords[keys]
+    #         nPts += 1
+    #     return nPts/nPaths
+
+    # def writeEff(self, routeFile, routeEff):
+    #     infoFile = os.path.join(self.pathDir, "info.txt")
+    #     if os.path.exists(infoFile):
+    #         writeMode = 'a'
+    #     else:
+    #         writeMode = 'w'
+    #     with open(infoFile, writeMode) as f:
+    #         routeName = routeFile.split('/')[-1]
+    #         f.write("\n{:s}: {:2.4f}".format(
+    #                 routeName,
+    #                 routeEff))
 
     def plot(self, ax, color='b'):
         # path
@@ -116,19 +186,18 @@ class Path(object):
         ax.scatter(cords[0, 0], cords[0, 1], color=color, marker='^')
         ax.scatter(cords[-2, 0], cords[-2, 1], color=color, marker='s')
         if self.homePt is not None:
-            #plot the home point as a 'o'
-            HomeUTMx, HomeUTMy  = utm.from_latlon(*self.homePt)[0:2]
+            # plot the home point as a 'o'
+            HomeUTMx, HomeUTMy = utm.from_latlon(*self.homePt)[0:2]
             ax.scatter(HomeUTMx, HomeUTMy, color=color, marker='o')
             ax.plot([HomeUTMx, cords[0, 0]], [HomeUTMy, cords[0, 1]],
-                     color=color, linestyle="--")
-
+                    color=color, linestyle="--")
 
     def write(self, filename, **kwargs):
         # writes the trajectory as a txt file
         # Lat,Long,Alt,Speed,Picture,ElevationMap,WP,CameraTilt,UavYaw,DistanceFrom
         # set flight parameters
-        spd =     kwargs["speed"]
-        alt =     kwargs["altitude"]
+        spd = kwargs["speed"]
+        alt = kwargs["altitude"]
         xferSpd = kwargs["xfer speed"]
         xferAlt = kwargs["xfer altitude"]
         xferAsc = kwargs["xfer ascend"]
@@ -153,18 +222,3 @@ class Path(object):
             # return home
             f.write(f"{Hlat}, {Hlng}, {xferAlt}, {xferDes},FALSE,,1\n")
             f.write(f"{Hlat}, {Hlng}, {landALt}, {xferDes},FALSE,,1\n")
-
-
-def main(pathDir):
-    # get path files
-    pathFiles = glob.glob(os.path.join(pathDir, "*/*/"))
-
-    # read json
-    # jsonFile = mission + ".json"
-    for pathDir in pathFiles:
-        Path(pathDir)
-
-
-if __name__ == '__main__':
-    pathDir = "../out"
-    main(pathDir)
