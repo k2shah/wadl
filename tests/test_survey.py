50cdc68467e7ba2b663a16aa7b3d0d3c3563f064
Kunal Shah <kshah.kunal@gmail.com>
2020-08-05 12:08:12 -0700

    deleted util file and added tests for bigger survey

--- tests/test_survey.py
+++ tests/test_survey.py
@@ -4,10 +4,13 @@ import os
 
 
 @pytest.fixture
-def island():
+def survey():
     """test solver class """
     # build survey test fixture
     from wadl.survey import Survey
+    solverParams = {"subGraph_size": 30,
+                    "SATBound_offset": 4
+                    }
 
     # make survey object
     path = os.path.join(os.path.dirname(__file__))
@@ -15,11 +18,12 @@ def island():
     if not os.path.exists(outDir):
         os.makedirs(outDir)
 
-    survey = Survey('test', outDir)
+    survey = Survey('test', outDir,
+                    solverParam=solverParams)
     return survey
 
 
-def test_plan(island):
+def test_island(survey):
     # get a island ("little norway")
     path = os.path.join(os.path.dirname(__file__), 'data')
     file = os.path.join(path, "Little Norway")
@@ -32,5 +36,22 @@ def test_plan(island):
                     "xfer_descend":  4,  # m/s
                     "land_altitude": 30,  # m
                     }
-    island.addTask(file, step=35, flightParams=flightParams)
-    island.plan(plot=False)
+    survey.addTask(file, step=35, flightParams=flightParams)
+    survey.plan(plot=False)
+
+
+def test_croz(survey):
+    # get a island ("little norway")
+    path = os.path.join(os.path.dirname(__file__), 'data')
+    file = os.path.join(path, "croz_west")
+    flightParams = {"limit":         12*60,  # s
+                    "speed":         5.0,  # m/s
+                    "altitude":      35.0,  # m
+                    "xfer_speed":    12.0,  # m/s
+                    "xfer_altitude": 70.0,  # m
+                    "xfer_ascend":   5,  # m/s
+                    "xfer_descend":  4,  # m/s
+                    "land_altitude": 30,  # m
+                    }
+    survey.addTask(file, roation=15, step=35, flightParams=flightParams)
+    survey.plan(plot=False)
