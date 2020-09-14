1a2b864d49e46a64f3096c0d538be86eceaece13
Kunal Shah <kshah.kunal@gmail.com>
2020-09-14 15:11:36 -0700

    Parameters (#23)
    * moved parameters to their own class
    
    * fixed tests
    
    * added an example

--- wadl/solver/solver.py
+++ wadl/solver/solver.py
@@ -1,23 +1,49 @@
 # gen
 import time
 # lib
-from wadl.solver.SATproblem import SATproblem
-from wadl.solver.metaGraph import MetaGraph
+from .SATproblem import SATproblem
+from .metaGraph import MetaGraph
+from ..lib.parameters import Parameters
 from tqdm import tqdm
+
+
+class SolverParameters(Parameters):
+    """docstring for """
+
+    def __init__(self, default=True):
+        super(SolverParameters, self).__init__(default)
+
+    def setDefaults(self):
+        self["subGraph_size"] = 40
+        self["SATBound_offset"] = 2
+        self["timeout"] = 60
+        self["maxProblems"] = 10
 
 
 class BaseSolver(object):
     """docstring for Solver"""
 
-    def __init__(self, **kwargs):
-        self.parameters = kwargs
+    def __init__(self, parameters=None):
+        self.parameters = parameters
+        if parameters is None:
+            self.parameters = SolverParameters(default=True)
+        else:
+            self.parameters = parameters
+
+    @property
+    def parameters(self):
+        return self._parameters
+
+    @parameters.setter
+    def parameters(self, value):
+        self._parameters = value
 
     def setup(self, graph):
         self.graph = graph
 
     def solve(self):
         # solve the problem
-        bound = len(self.graph) + self.parameters["SATBound_offset"]
+        bound = len(self.graph) + self._parameters["SATBound_offset"]
         problem = SATproblem(self.graph, bound)
         return problem.solve()
 
@@ -25,28 +51,26 @@ class BaseSolver(object):
 class LinkSolver(BaseSolver):
     """docstring for LinkSolver"""
 
-    def __init__(self, subGraph_size=40, SATBound_offset=2):
-        super(LinkSolver, self).__init__()
-        self.parameters["subGraph_size"] = subGraph_size
-        self.parameters["SATBound_offset"] = SATBound_offset
+    def __init__(self, parameters=None):
+        super(LinkSolver, self).__init__(parameters)
 
     def setup(self, graph):
         # setup graph
         self.metaGraph = MetaGraph(graph,
-                                   size=self.parameters["subGraph_size"])
+                                   size=self._parameters["subGraph_size"])
 
     def solve(self, routeSet):
         subPaths = []
         startTime = time.time()
         for i, graph in enumerate(tqdm(self.metaGraph.subGraphs, ascii=True)):
-            bound = len(graph) + self.parameters["SATBound_offset"]
+            bound = len(graph) + self._parameters["SATBound_offset"]
             print(f"\tbuilding problem {i}")
             problem = SATproblem(graph, bound)
             counter = 0
             solved = False
             sTime = time.time()
             while not solved:
-                if problem.solve(timeout=60):
+                if problem.solve(timeout=self.parameters["timeout"]):
                     solved = True
                     print(f"\t\tsolved in {time.time()-sTime} sec")
                     subPaths.append(problem.output()[0])
@@ -57,7 +81,7 @@ class LinkSolver(BaseSolver):
                     counter += 1
                     solved = False
                 # check counter
-                if counter > 10:
+                if counter > self.parameters["maxProblems"]:
                     raise RuntimeError(f"\tproblem {i} critically infeasible. "
                                        "graph may be degenerate")
 
